e26a3259325a8c36bc9276fa4722f700c3480e93
rabby <d00197750@student.dkit.ie>
2020-04-18 16:04:11 +0000

    Remove unnecessary comments

--- main.c
+++ main.c
@@ -4,27 +4,13 @@
 
 int main(int argc, char** argv) {
 
-	//char str[100];
-
-	  //char buffer [33];
-	  
-
-	//scanf("%s",str);
-
-//	   printf( "\n YOU ENTERED %s", str);
-	//fgets(str,sizeof str,stdin);
-
 	for(int i = 1; i < argc; ++i)
 	{
-		//printf("wfeef");
 		if(strlen(argv[i]) == 7 || strlen(argv[i]) == 9)
 		{
 			printf("%s\n",argv[i]);
 		}
-		//printf("%s\n",argv[0]);
-		//}
 	}
-
 
 	return 0;
 }
