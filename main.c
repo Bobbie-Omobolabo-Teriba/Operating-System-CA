27eda16de644074751506da7adc22bef8b6aef53
rabby <d00197750@student.dkit.ie>
2020-04-18 14:57:01 +0000

    Validate 6 digit and 8 digit hex

--- main.c
+++ main.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main() {
+int main(int argc, char** argv) {
 
-	char str[100];
-	int i;
+	//char str[100];
 
-	scanf("%s", str);
-	printf( "\nYou entered: %s\n", str);
+	  //char buffer [33];
+	  
+
+	//scanf("%s",str);
+
+//	   printf( "\n YOU ENTERED %s", str);
+	//fgets(str,sizeof str,stdin);
+
+	for(int i = 1; i < argc; ++i)
+	{
+		//printf("wfeef");
+		if(strlen(argv[i]) == 7 || strlen(argv[i]) == 9)
+		{
+			printf("%s\n",argv[i]);
+		}
+		//printf("%s\n",argv[0]);
+		//}
+	}
+
 
 	return 0;
 }
