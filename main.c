25a8d69b202e5d9c845b25a9710fbf28eb09ef59
Bobbie-Omobolabo-Teriba <bobbyteriba@yahoo.ie>
2020-04-20 19:59:51 +0100

    Hex to RGBA conversion code in main.c for section 2, task 3

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
 
 int main(int argc, char** argv) {
-
 	for(int i = 1; i < argc; ++i)
 	{
 		if(strlen(argv[i]) == 7 || strlen(argv[i]) == 9)
@@ -11,6 +12,24 @@ int main(int argc, char** argv) {
 			printf("%s\n",argv[i]);
 		}
 	}
+
+
+//  char* hexdec = "FF";
+  // int decimal = 0;
+
+  // int length = strlen(hexdec);
+  // for(int i = 0; i < length; i++){
+    // char digit = hexdec[length - 1 - i];
+
+    // if(digit >= '0' && digit <= '9'){
+      // decimal += (digit - '0') * pow(16,i);
+    // }
+    // else{
+    // decimal += (digit - 'A' + 10) * pow(16,i);
+   //  }
+  // }
+
+  // printf("Dec %d", decimal);
 
 	return 0;
 }
